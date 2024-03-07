3ee119243308f3e0e8a2bc311ad0b78815ea072f
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-08 01:11:30 +0530

    Day 44 Display Account members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,14 @@
 class MembersController < ApplicationController
+  before_action :set_current_account
+
+  def index
+    @member = @current_account.members
+  end
+  
+
+  private
+
+  def set_current_account
+    @current_account = Account.find(params[:account_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :members
-  devise_for :users
+  resources :accounts do
+    resources :members
+  end
   resources :projects
-  resources :accounts
+  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
