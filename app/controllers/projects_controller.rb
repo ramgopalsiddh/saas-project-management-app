222ea95f17d6cb972930fd353c73c2d5cf5d5ec6
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-12 15:51:59 +0530

    Day 48 Add rescue for when no tenant error (ActsAsTenant::Errors::NoTenantSet)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,13 @@ class ProjectsController < ApplicationController
 
   # GET /projects or /projects.json
   def index
-    @projects = current_user.projects
+    begin
+      @projects = current_user.projects
+      # show errer if you don't have tenant of 
+    rescue ActsAsTenant::Errors::NoTenantSet
+      flash[:alert] = "You do not have a tenant. Please set a tenant before access."
+      redirect_to root_path
+    end
   end
 
   # GET /projects/1 or /projects/1.json
