a659b746c4d036104ae002eb9cf724e9e89ee9ba
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-14 15:42:43 +0530

    Day 50 Add Stripe initializers and other setup for stripe

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,5 +3,6 @@ class Account < ApplicationRecord
   validates_presence_of :domain, :subdomain
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :projects, dependent: :destroy
-
+  has_one :payment
+  accepts_nested_attributes_for :payment
 end
