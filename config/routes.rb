8c80b091d05814629807bf1317ed57603ac002d5
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-15 02:44:31 +0530

    Day 51 Updates for setup stripe

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :artifacts
   end
   resources :accounts
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
