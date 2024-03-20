1d8b3eac38878fcd4437487e28f567d242c12788
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-03-20 18:43:49 +0530

    Day 56 Add first & last name in users form and update devise's (authantication) views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_account_update_params, only: [:update, :create]
+
   def create
     build_resource(sign_up_params)
 
@@ -27,6 +29,12 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   private
+
+  # Permit first & last name in devise 
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 
   def create_payment_and_account(user)
     Payment.transaction do
